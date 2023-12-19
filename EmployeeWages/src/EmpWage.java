2253e93bb4c275a962bb0b20763b402274e90a26
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-20 01:20:19 +0530

    usage of switch case and monthly wage calculation

--- EmployeeWages/src/EmpWage.java
+++ EmployeeWages/src/EmpWage.java
@@ -1,37 +1,57 @@
 public class EmpWage {
+    static int WAGE_PER_HOUR = 20;
 
-    public static void main(String[] args) {
-
-        // UC1
-        int empCheck = Math.random() < 0.5 ? 0 : 1;
-
-        if (empCheck == 1)
-            System.out.println("Employee is Present");
-        else{
-            System.out.println("Employee is Absent");
-            return;
-        }
-
-        // UC2 && UC3
-        int WAGE_PER_HOUR = 20;
+    // UC2 && UC3 daily wage calculation
+    public static int hoursPerDay() {
 
         int empWage = 0;
         int empHour = 0;
+
         int isFullTime = Math.random() < 0.5 ? 0 : 1;
-        if (empCheck == 1) {
-           if (isFullTime == 0) {
-                System.out.println("Part Time Employee");
-                empHour = 4;
+
+        if (isPresent() == true) {
+            switch (isFullTime) {
+                case 1:
+                    System.out.println("Employee is fulltime");
+                    empHour = 8;
+                    break;
+
+                default:
+                    System.out.println("Employee is parttime");
+                    empHour = 4;
+                    break;
             }
-            else{
-                System.out.println("Full Time Employee");
-                empHour = 8;
-            }
-            
+
         }
 
         empWage = empHour * WAGE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
+        // System.out.println("Emp Wage: " + empWage);
+
+        return empHour;
+    }
+
+    // UC1 && UC4
+    public static boolean isPresent() {
+        int empCheck = Math.random() < 0.5 ? 0 : 1;
+
+        switch (empCheck) {
+            case 1:
+                return true;
+
+            default:
+                return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        // UC5 monthly wage calculation
+        int monthlyWage = 0;
+        for (int i = 0; i < 20; i++) {
+            monthlyWage += WAGE_PER_HOUR * hoursPerDay();
+        }
+        System.out.println("Monthly Wage is:" + monthlyWage);
 
 
     }
